8e2945900623bebfebd46845814fa4b190ffb154
ruslan.badretdinov1111 <ruslan.badretdinov1111@yandex.ru>
2021-12-24 23:44:02 +0500

    Изменил методы getWeight() и getPrice() - чтобы возвращали значение веса (до этого были void)

--- src/Homework001_PresentBox/BoxProperties.java
+++ src/Homework001_PresentBox/BoxProperties.java
@@ -4,9 +4,11 @@ public interface BoxProperties {
     void add(Sweetness sweetness);
     void remove(int index);
     void removeLast();
-    void getWeight();
-    void getPrice();
+    float getWeight();
+    float getPrice();
     void getInfoAboutAllObjects();
     void removeTheSmallestPrice();
     void removeTheSmallestWeight();
+    void optimizeByBoxWeight(float weight);
+    void optimizeByBoxPrice(float weight);
 }
